07c16cc309d29d93a48e6999f93eedee0e0163e3
gukun00 <77414@qq.com>
2017-01-06 12:01:29 +0800

    j
    x

--- hello.js
+++ hello.js
@@ -1,8 +1,37 @@
 var http = require("http");
+var util= require("util");
+var url = require("url");
+
+//var process = require("process");
+//console.log(process)
 var a = 123;
 var cc = function() {
   a = 1;
   b = 2;
 }
 
-console.log(1)
+//console.log(1)
+
+function doSomething(callback) {
+  console.log(1);
+  var i = 0;
+  for (var i = 0; i < 100; i++) {
+    j = i / 2;
+  }
+  console.log(i);
+  callback();
+  process.nextTick(callback);
+
+  console.log(1111);
+}
+doSomething(function() {
+  console.log(2);
+});
+
+
+http.createServer(function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('123123213213');
+}).listen(3000);
